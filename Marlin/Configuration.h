2f985275545cf446ab3ea1a9fbd2a890640daafa
Maximilian Rehms <rehms.maximilian@gmail.com>
2025-01-18 17:08:26 +0100

    reduced x axix travel for new belt tensioner

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1378,7 +1378,7 @@
 #define X_MIN_POS 0
 #define Y_MIN_POS -18.5
 #define Z_MIN_POS 0
-#define X_MAX_POS X_BED_SIZE +14
+#define X_MAX_POS X_BED_SIZE +10
 #define Y_MAX_POS Y_BED_SIZE -18
 #define Z_MAX_POS 250
 //#define I_MIN_POS 0
