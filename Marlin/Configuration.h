655fbfa9500033919aaf5706c8668f868a5e7f84
Maximilian Rehms <rehms.maximilian@gmail.com>
2025-11-23 09:46:35 +0100

    update axis steps per unit and nozzle to probe offset for improved accuracy

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -928,7 +928,7 @@
  * Override with M92
  *                                      X, Y, Z [, I [, J [, K]]], E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 436.84 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 485.38 }
 
 /**
  * Default Max Feed Rate (mm/s)
@@ -1182,7 +1182,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { -55, -13, -1.80 }
+#define NOZZLE_TO_PROBE_OFFSET { -55, -13, -1.70 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
